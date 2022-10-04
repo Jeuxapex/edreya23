5d8404abd7befb21231f2e1036824ae569e5112c
Jeuxapex <36998142+Jeuxapex@users.noreply.github.com>
2022-10-04 16:18:32 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,6 @@ import AnimatedText from "./AnimatedText";
 const homeData = {
   firstName: "Burak",
   lastName: "Tekelioğlu",
-  skills: ["Fotoğrafçı"],
 };
 
 const Home = ({ activeWithBtn }) => {
@@ -20,7 +19,7 @@ const Home = ({ activeWithBtn }) => {
             {homeData.firstName}{" "}
             <span className="coloring">{homeData.lastName}</span>
           </h3>
-          <h3 className="job">
+          <h3>Art
             <AnimatedText />
           </h3>
           <div className="edrea_tm_button transition_link">
