add6c5c51d373097458b120d1f63e6f5a7b5b02c
Jeuxapex <36998142+Jeuxapex@users.noreply.github.com>
2022-10-04 17:21:57 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,12 +15,6 @@ const Home = ({ activeWithBtn }) => {
     <div className="edrea_tm_section animated" id="home">
       <div className="section_inner">
         <div className="edrea_tm_home">
-        <div className="image">
-                <img src="img/thumbs/1-1.jpg" alt="" />
-                <div className="main" data-img-url="img/1.jpg" />
-              </div>
-    </div>
-    
           <h3 className="name">
             {homeData.firstName}{" "}
             <span className="coloring">{homeData.lastName}</span>
