520098b9d9d3bbb9ab85139e1c11962ded97baa9
Jeuxapex <36998142+Jeuxapex@users.noreply.github.com>
2022-10-04 16:52:47 +0300

    Update AboutPopup.js

--- src/components/popup/AboutPopup.js
+++ src/components/popup/AboutPopup.js
@@ -25,9 +25,6 @@ const AboutPopup = ({ open, close, aboutData }) => {
                   {aboutData.firstName}{" "}
                   <span className="coloring">{aboutData.lastName}</span>
                 </h3>
-                <h3 className="job">
-                  <AnimatedText />
-                </h3>
               </div>
             </div>
             <div className="list">
@@ -89,145 +86,22 @@ const AboutPopup = ({ open, close, aboutData }) => {
               <div className="about_title">
                 <h3>
                   <span>
-                    About <span className="coloring">Me</span>
+                    Ben <span className="coloring">kimim?</span>
                   </span>
                 </h3>
               </div>
               <div className="text">
                 <p>
-                  Hello everybody! My name is <span>David Parker.</span> I am a
-                  graphic designer, and {`I'm`} very passionate and dedicated to
-                  my work. With 20 years experience as a professional a graphic
-                  designer, I have acquired the skills and knowledge.
+                  1970 yılında Muğla’da doğan R.Burak Tekelioğlu ilk, orta ve lise öğrenim hayatını Muğla’da tamamladı. Üniversite öğrenimine Bilkent Üniversitesi GSF İç Mimarlık bölümünde başlayıp Yeditepe Üniversitesi GSF İç Mimarlık bölümünden mezun oldu.Uzun yıllar mimari projelerde yer alarak İç mimarlık mesleğine tasarımları ile devam etti.  Muammer Yanmaz Atölyesi ’40 Haramiler’ topluluğunda profesyonel fotoğrafçılık eğitimi aldı. Yaklaşık 3 yıl boyunca yurtiçi ve yurtdışı  sokak portresi alanında çekimler yaptı.Sonrasında üniversite yıllarında başlayan, kendi yarattığı karakter çizimleri ile çağdaş sanatı birleştirerek sentezlediği resim ve heykel çalışmalarına ağırlık verdi. 2018 yılında Urla Sanat Sokağında ‘DEBURKY’ Sanat Atölyesini kurdu. Yurtiçi ve yurtdışı resim ve seramik eserlerinin  yer aldığı sergilere katıldı. Sanat çalışmalarında varoluş , bilinçaltı ve insan, zaman düşleri ,şehir ve yalnızlık  ,kökler gibi öznel duyuları kendine özgü yorumladı.Sanatçı çalışmalarına Urla’da aynı zamanda sanat galerisi olan ‘Deburky Sanat Atölyesinde’ devam etmektedir.
                 </p>
               </div>
             </div>
-            <div className="service">
-              <div className="about_title">
-                <h3>
-                  <span>
-                    Quality <span className="coloring">Services</span>
-                  </span>
-                </h3>
-              </div>
-              <div className="list">
-                <ul>
-                  {aboutData.serviceLists &&
-                    aboutData.serviceLists.map((service, i) => (
-                      <li key={i}>
-                        <i className="icon-right-dir" />
-                        {service}
-                      </li>
-                    ))}
-                </ul>
-              </div>
-            </div>
-            <div className="prog_skill">
-              <div className="about_title">
-                <h3>
-                  <span>
-                    Programming <span className="coloring">Skills</span>
-                  </span>
-                </h3>
-              </div>
-              <div className="oki_progress">
-                {aboutData.skills &&
-                  aboutData.skills.programming &&
-                  aboutData.skills.programming.map((programming, i) => (
-                    <div
-                      key={i}
-                      className="progress_inner skillsInner___"
-                      data-value={95}
-                    >
-                      <span>
-                        <span className="label">{programming.name}</span>
-                        <span
-                          className="number"
-                          style={{ right: `${100 - programming.value}%` }}
-                        >
-                          {programming.value}%
-                        </span>
-                      </span>
-                      <div className="background">
-                        <div className="bar open">
-                          <div
-                            className="bar_in"
-                            style={{ width: `${programming.value}%` }}
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-              </div>
-            </div>
-            <div className="lang_skill">
-              <div className="about_title">
-                <h3>
-                  <span>
-                    Language <span className="coloring">Skills</span>
-                  </span>
-                </h3>
-              </div>
-              <div className="circular_progress_bar">
-                <ul>
-                  {aboutData.skills &&
-                    aboutData.skills.language &&
-                    aboutData.skills.language.map((language, i) => (
-                      <li key={i}>
-                        <div className="list_inner">
-                          <div className="myCircle" data-value="0.95">
-                            <CircularProgressbar
-                              value={language.value}
-                              text={`${language.value}%`}
-                              strokeWidth={2}
-                              styles={buildStyles({
-                                // Colors
-                                pathColor: color,
-                              })}
-                            />
-                          </div>
-                          <div className="title">
-                            <span>{language.name}</span>
-                          </div>
-                        </div>
-                      </li>
-                    ))}
-                </ul>
-              </div>
-            </div>
+  
             <div className="timeline">
               <div className="about_title">
                 <h3>
                   <span>
-                    Education <span className="coloring">Timeline</span>
-                  </span>
-                </h3>
-              </div>
-              <div className="list">
-                <ul>
-                  {aboutData &&
-                    aboutData.education &&
-                    aboutData.education.map((edu, i) => (
-                      <li key={i}>
-                        <div className="list_inner">
-                          <div className="time">
-                            <span>{edu.year}</span>
-                          </div>
-                          <div className="place">
-                            <h3>{edu.unv}</h3>
-                            <span>{edu.degree}</span>
-                          </div>
-                        </div>
-                      </li>
-                    ))}
-                </ul>
-              </div>
-            </div>
-            <div className="timeline">
-              <div className="about_title">
-                <h3>
-                  <span>
-                    Working <span className="coloring">Timeline</span>
+                    Sergiler <span className="coloring"></span>
                   </span>
                 </h3>
               </div>
@@ -251,14 +125,7 @@ const AboutPopup = ({ open, close, aboutData }) => {
                 </ul>
               </div>
             </div>
-            <div className="partners">
-              <div className="about_title">
-                <h3>
-                  <span>
-                    My <span className="coloring">Partners</span>
-                  </span>
-                </h3>
-              </div>
+           
               <div className="list">
                 <ul>
                   {aboutData &&
@@ -272,89 +139,6 @@ const AboutPopup = ({ open, close, aboutData }) => {
                       </li>
                     ))}
                 </ul>
-              </div>
-            </div>
-            <div className="testimonial">
-              <div className="about_title">
-                <h3>
-                  <span>
-                    Clients <span className="coloring">Feedback</span>
-                  </span>
-                </h3>
-              </div>
-              <div className="list">
-                <Swiper {...testimonialSlider} className="owl-carousel">
-                  <SwiperSlide>
-                    <div className="list_inner">
-                      <div className="text">
-                        <i className="icon-quote-left" />
-                        <p>
-                          Beautiful minimalist design and great, fast response
-                          with support. Highly recommend. Thanks Marketify!
-                        </p>
-                      </div>
-                      <div className="details">
-                        <div className="image">
-                          <div
-                            className="main"
-                            data-img-url="img/testimonials/1.jpg"
-                          />
-                        </div>
-                        <div className="info">
-                          <h3>Alexander Walker</h3>
-                          <span>Graphic Designer</span>
-                        </div>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="list_inner">
-                      <div className="text">
-                        <i className="icon-quote-left" />
-                        <p>
-                          These people really know what they are doing! Great
-                          customer support availability and supperb kindness.
-                        </p>
-                      </div>
-                      <div className="details">
-                        <div className="image">
-                          <div
-                            className="main"
-                            data-img-url="img/testimonials/2.jpg"
-                          />
-                        </div>
-                        <div className="info">
-                          <h3>Armin Van Buuren</h3>
-                          <span>Content Manager</span>
-                        </div>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="list_inner">
-                      <div className="text">
-                        <i className="icon-quote-left" />
-                        <p>
-                          I had a little problem and the support was just
-                          awesome to quickly solve the situation. And keep going
-                          on.
-                        </p>
-                      </div>
-                      <div className="details">
-                        <div className="image">
-                          <div
-                            className="main"
-                            data-img-url="img/testimonials/3.jpg"
-                          />
-                        </div>
-                        <div className="info">
-                          <h3>Baraka Clinton</h3>
-                          <span>English Teacher</span>
-                        </div>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                </Swiper>
               </div>
             </div>
           </div>
