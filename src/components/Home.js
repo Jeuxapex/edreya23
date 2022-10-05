80ea74d771ccdad73090a5361e332937ad90af65
Jeuxapex <36998142+Jeuxapex@users.noreply.github.com>
2022-10-05 14:29:02 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,6 +13,10 @@ const Home = ({ activeWithBtn }) => {
 
   return (
     <div className="edrea_tm_section animated" id="home">
+     <div className="image">
+      <img src="img/thumbs/1-1.jpg" alt="" />
+      <div className="main" data-img-url="img/1.jpg" />
+      </div>
       <div className="section_inner">
         <div className="edrea_tm_home">
           <h3 className="name">
@@ -21,38 +25,12 @@ const Home = ({ activeWithBtn }) => {
           </h3>
           <h3>Art has no rules so do i.
           </h3>
-              <div className="edrea_tm_section hidden animated" id="about">
-        <div className="section_inner">
-          <div className="edrea_tm_about">
-            <div className="left">
-              <div className="image">
-                <img src="img/thumbs/1-1.jpg" alt="" />
-                <div className="main" data-img-url="img/1.jpg" />
-              </div>
-            </div>
-            <div className="right">
-              <div className="short">
-                <h3 className="name">
-                  {aboutData.firstName}{" "}
-                  <span className="coloring">{aboutData.lastName}</span>
-                </h3>
-                <h3>Art has no rules so do i.
-                </h3>
-              </div>
-              <div className="text">
-                <p>
-                  1970 yılında Muğla’da doğan <span>R.Burak Tekelioğlu</span> ilk, orta ve lise öğrenim hayatını Muğla’da tamamladı. Üniversite öğrenimine Bilkent Üniversitesi GSF İç Mimarlık bölümünde başlayıp Yeditepe Üniversitesi GSF İç Mimarlık bölümünden mezun oldu.Uzun yıllar mimari projelerde yer alarak İç mimarlık mesleğine tasarımları ile devam etti.  Muammer Yanmaz Atölyesi ’40 Haramiler’ topluluğunda profesyonel fotoğrafçılık eğitimi aldı. 
-                </p>
-              </div>
-              <div className="edrea_tm_button">
-                <a href="#" onClick={() => setPopup(true)}>
-                  Devamını gör
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+             <div className="edrea_tm_button transition_link">
+            <a href="#contact" onClick={() => changeNav("contact")}>
+              Get in Touch
+            </a>
       </div>
+          </div>
         </div>
       </div>
     </div>
